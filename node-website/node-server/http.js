3e32eb803eb272838de9c6c46fd219270fa06a14
stone <1484542698@qq.com>
2019-07-20 11:02:28 +0800

    feat(node-server): start learning node.js

---
+++ node-website/node-server/http.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const url = require('url')
+
+const server = http.createServer((req, res) => {
+    parseUrl(req)
+    // 设置响应头
+    res.writeHead(200, {
+        'Content-Type': 'text/html;chartset=utf-8'
+    })
+
+    // 往页面打印值
+    res.write('<h1 style="text-align:center">hello nodejs</h1>')
+
+    // 结束响应
+    res.end()
+})
+server.listen(3000)
+
+function parseUrl(request) {
+    if(request.url !== '/favicon.ico') {
+        const result = url.parse(request.url, true)
+        console.log(result)
+        console.log(result.query.name)
+        console.log(result.query.school)
+    }
+}
